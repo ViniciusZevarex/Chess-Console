539598815e32e96620afc90f1caa6e0b649ee7b7
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2020-05-05 15:49:43 -0300

    Criação da classe board.

---
+++ ChessConsole/board/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace board
+{
+    class Position
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Row + ", " + Column;
+        }
+
+    }
+}
