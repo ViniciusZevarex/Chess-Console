512dba2592c123abec2980c70230172db643fa69
ViniciusZevarex <vinicius.zevarex2002@gmail.com>
2020-05-05 15:58:28 -0300

    Inicio da implementação da classe Board, Piece, Position e o enum Color

---
+++ ChessConsole/board/Board.cs
@@ -0,0 +1,16 @@
+﻿namespace board
+{
+    class Board
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Piece[,] Pieces;
+
+        public Board(int row, int column, Piece[,] pieces)
+        {
+            Row = row;
+            Column = column;
+            Pieces = new Piece[row, column];
+        }
+    }
+}
---
+++ ChessConsole/board/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Emit;
+
+namespace board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; set; }
+        public int AmtMoviments { get; protected set; }
+        public Board Board { get; set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            Position = position;
+            Color = color;
+            AmtMoviments = 0;
+            Board = board;
+        }
+    }
+}
